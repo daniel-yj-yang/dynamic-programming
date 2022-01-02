a6cc3c35e50b703a79a58d5e3bc2abc2df4e8dcf
Daniel Yang <3014022+daniel-yj-yang@users.noreply.github.com>
2022-01-02 14:33:02 -0600

    Update _Fibonacci_numbers.py

--- DP/Fibonacci/_Fibonacci_numbers.py
+++ DP/Fibonacci/_Fibonacci_numbers.py
@@ -7,15 +7,16 @@
 
 def Fibonacci_numbers():
   print("1. recurrence relation (subproblem): Fn = Fn-1 + Fn-2")
-  print("2. Bottom-up DP, Time Complexity O(n)")
+  print("2. Bottom-up DP, Time Complexity O(n), Space Complexity O(1)")
   print("""
 def fib(n):
     F = [0, 1]    
     if n < 2:
         return F[n]
     for i in range(2, n+1):
-        F_i = sum(F)
+        F_i = sum(F) # implementation of the recurrence relation: Fn = Fn-1 + Fn-2
         F[:] = F[1:] + [F_i]
     return F_i
-)""")
+)
+""")
 
