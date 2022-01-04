7f12ff12d642b81775854cefacc1cf9e34b287a1
Daniel Yang <3014022+daniel-yj-yang@users.noreply.github.com>
2022-01-04 04:59:11 -0600

    Update

--- DP/problems/_Best_Time_to_Buy_and_Sell_Stock.py
+++ DP/problems/_Best_Time_to_Buy_and_Sell_Stock.py
@@ -17,14 +17,14 @@ class Best_Time_to_Buy_and_Sell_Stock():
         print(f"(1) min_price_past_and_now(i) = min(prices[i], min_price_past_and_now(i-1)), and max_profit_past_and_now = max(max_profit_past_and_now, prices[i] - min_price_past_and_now(i)), while counting from prices[0] forward;\n")
         print(f"(2) max_price_now_n_future(i) = max(prices[i], max_price_now_n_future(i+1)), and max_profit_now_n_fugure = max(max_profit_now_n_fugure, max_price_now_n_future(i) - prices[i]), while counting from prices[-1] backward;\n")
         print(f"(3) max_profit(i) = max(price_diff[i], price_diff[i] + max_profit(i-1)), for i in range(1, len(prices)), where price_diff[i] = prices[i] - prices[i-1]; this is basically to find the max. subarray sum with the array being np.diff(prices);\n")
-        print(f"(4) max_profit(i) = max(0,             price_diff[i] + max_profit(i-1)), while all else the same as in (3); this is because if price_diff[i] < 0, it is better off not having that transaction at all to ensure that max_profit(i) >= 0;\n")
+        print(f"(4) max_profit(i) = max(0,             price_diff[i] + max_profit(i-1)), everything else the same as in (3); this is because if price_diff[i] < 0, it is better off not having any transactions at all to ensure that max_profit(i) >= 0;\n")
         print(inspect.getsource(self.top_down) + '\n')
         print(inspect.getsource(self.bottom_up))
 
     def top_down(self, prices=[7, 1, 5, 3, 6, 4, 2, 10, 2, 3, 9, 1]):
         """
         Recursion + Memoization;
-        Time Complexity O(n), Space Complexity O(n) for the recursion call stack
+        Time Complexity O(n), Space Complexity O(n) for the recursion call stack;
         this is implementing #4 recurrence relation above
         """
         @lru_cache(maxsize=None)
@@ -41,8 +41,8 @@ class Best_Time_to_Buy_and_Sell_Stock():
 
     def bottom_up(self, prices=[7, 1, 5, 3, 6, 4, 2, 10, 2, 3, 9, 1]):
         """
-        Tabulation - full tabulation has Space Complexity O(n); keeping only needed items has Space Complexity O(1)
-        Time Complexity O(n), Space Complexity O(1)
+        Tabulation - full tabulation has Space Complexity O(n); keeping only needed items has Space Complexity O(1);
+        Time Complexity O(n), Space Complexity O(1);
         this is implementing #4 recurrence relation above
         """
         global_max = max_profit = 0
--- tests/test.py
+++ tests/test.py
@@ -11,6 +11,7 @@ F.explanation()  # this will show the code and some explanations
 print(F.top_down(n = 500))
 print(F.bottom_up(n = 500))
 
+print('---------------------------------------------------------------------------------------')
 
 from DP import House_Robber as robber
 r = robber()
@@ -18,6 +19,7 @@ r.explanation()
 print(r.top_down(nums = [3, 10, 3, 1, 2, 4, 10, 2, 44, 98]))
 print(r.bottom_up(nums = [3, 10, 3, 1, 2, 4, 10, 2, 44, 98]))
 
+print('---------------------------------------------------------------------------------------')
 
 from DP import Min_Cost_Climbing_Stairs as climb
 c = climb()
@@ -25,9 +27,18 @@ c.explanation() # this will show the code and some explanations
 print(c.top_down(cost = [3, 10, 3, 1, 2, 4, 10, 2, 44, 98]))
 print(c.bottom_up(cost = [3, 10, 3, 1, 2, 4, 10, 2, 44, 98]))
 
+print('---------------------------------------------------------------------------------------')
 
 from DP import Maximum_Subarray as maxsub
 m = maxsub()
 m.explanation() # this will show the code and some explanations 
 print(m.top_down(nums=[-2, 1, -3, 4, -1, 2, 1, -5, 4, 9, -2, 10, 15, -3, -4]))
 print(m.bottom_up(nums=[-2, 1, -3, 4, -1, 2, 1, -5, 4, 9, -2, 10, 15, -3, -4]))
+
+print('---------------------------------------------------------------------------------------')
+
+from DP import Best_Time_to_Buy_and_Sell_Stock as stock
+s = stock()
+s.explanation()  # this will show the code and some explanations
+print(s.top_down(prices=[7, 1, 5, 3, 6, 4, 2, 10, 2, 3, 9, 1]))
+print(s.bottom_up(prices=[7, 1, 5, 3, 6, 4, 2, 10, 2, 3, 9, 1]))
