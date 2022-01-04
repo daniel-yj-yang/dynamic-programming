b807475e41c4d66c27fcd791e8bb80742fa452d4
Daniel Yang <3014022+daniel-yj-yang@users.noreply.github.com>
2022-01-04 05:31:53 -0600

    Update _Fibonacci_Numbers.py

--- DP/problems/_Fibonacci_Numbers.py
+++ DP/problems/_Fibonacci_Numbers.py
@@ -13,7 +13,7 @@ class Fibonacci_Numbers():
 
     def explanation(self):
         print(f"\nThe question provides an integer n and asks to return the n-th number in the Fibonacci sequence, with f(0) = 0, f(1) = 1")
-        print(f"\nThe recurrence relation: f(i) = f(i-2) + f(i-1), which means an automatic choice to add f(i-2) and f(i-1) for f(i), where f(i) is the i-th # in the Fibonacci sequence, with f(0) = 0, f(1) = 1\n\nSource codes:\n")
+        print(f"\nThe recurrence relation: f(i) = f(i-2) + f(i-1), which means an only choice to add f(i-2) and f(i-1) for f(i), where f(i) is the i-th # in the Fibonacci sequence, with f(0) = 0, f(1) = 1\n\nSource codes:\n")
         print(inspect.getsource(self.top_down) + '\n')
         print(inspect.getsource(self.bottom_up))
 
